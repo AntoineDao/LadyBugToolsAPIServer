045ab28ac7e3924ed1754309f37038ec493dc934
AntoineDao <antoinedao1@gmail.com>
2018-09-11 20:13:06 +0100

    moved postgres_utils and created db connection routine on server startup

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -1,11 +1,55 @@
 #!/usr/bin/env python3
 
 import connexion
-
 from swagger_server import encoder
+import os
+import sqlalchemy
+from swagger_server.postgres_utils import schemas
 
+def create_database(user, pword, server_ip, port, database_name):
+    """Create a database on the PostgreSQL server
 
-def main():
+    :param user: name of the connection user
+    :param pword: password of the db user
+    :param server_ip: ip address of the machine hosting the server
+    :param port: port the server is exposed on
+    :param database_name: name of the database
+
+    :return None
+    """
+    engine = sqlalchemy.create_engine("postgresql://{user}:{pword}@{server_ip}:{port}" \
+                                  .format(user=user, pword=pword, server_ip=server_ip, port=port))
+    conn = engine.connect()
+    conn.execute("commit")
+    conn.execute("create database {database_name}".format(database_name=database_name))
+    conn.close()
+
+def create_tables(user, pword, server_ip, port, database_name):
+    """Create a database on the PostgreSQL server
+
+    :param user: name of the connection user
+    :param pword: password of the db user
+    :param server_ip: ip address of the machine hosting the server
+    :param port: port the server is exposed on
+    :param database_name: name of the database
+
+    :return None
+    """
+    engine = sqlalchemy.create_engine("postgresql://{user}:{pword}@{server_ip}:{port}/{database_name}" \
+                                  .format(user=user, pword=pword, server_ip=server_ip, port=port, database_name=database_name))
+
+    conn = engine.connect()
+    schemas.Base.metadata.create_all(conn)
+    conn.close()
+
+def main(user, pword, server_ip, port, database_name):
+    try:
+        create_database(user, pword, server_ip, port, database_name)
+    except:
+        pass
+
+    create_tables(user, pword, server_ip, port, database_name)
+
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'LadybugTools API'})
@@ -13,4 +57,10 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    user = os.getenv("DB_USER")
+    pword = os.getenv("PASSWORD")
+    server_ip = os.getenv("SERVER_IP")
+    port = os.getenv("PORT")
+    database_name = os.getenv("DATABASE_NAME")
+
+    main(user, pword, server_ip, port, database_name)
